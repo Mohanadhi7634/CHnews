1840499850ba71731eb7bc0098421ca85a5d8ae2
mohanadhi!096 <mohanadhi096@gmail.com>
2023-08-10 15:17:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Home } from './Components/Home';
+
+import { NavBar } from './Components/NavBar';
+import { Politics } from './Components/Politics';
+import { Crime } from './Components/Crime';
+import { Tech } from './Components/Tech';
+import { Sports } from './Components/Sports';
+import { Cinema } from './Components/Cinema';
+import { Health } from './Components/Health';
+import { Admin } from './Components/Admin';
+import { Article } from './Components/Article';
+import { NotFound } from './Components/NotFound';
+
+
+
+
+function App() {
+  return (
+ <>
+   <NavBar />
+
+ <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/politics' element={<Politics />} ></Route>
+    <Route path='/crime' element={<Crime />} ></Route>
+    <Route path='/tech' element={<Tech />} ></Route>
+    <Route path='/sports' element= {<Sports />} ></Route>
+    <Route path='/cinema' element={<Cinema />} ></Route>
+    <Route path='/health' element ={<Health />}></Route>
+    <Route path='/admin' element ={<Admin />}></Route>
+    <Route path='/article/:id' element={<Article />}></Route>
+    <Route path='/politics/article/:id' element={<Article />}></Route>
+    <Route path='/crime/article/:id' element={<Article />}></Route>
+    <Route path='/tech/article/:id' element={<Article />}></Route>
+    <Route path='/sports/article/:id' element={<Article />}></Route>
+    <Route path='/cinema/article/:id' element={<Article />}></Route>
+    <Route path='/health/article/:id' element={<Article />}></Route>
+    <Route path="*" Component={NotFound} ></Route>
+
+  </Routes>
+ </>
+  );
+}
+
+export default App;
---
+++ src/Components/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Politics } from './Politics'
+import { Crime } from './Crime'
+import { Tech } from './Tech'
+import { Sports } from './Sports'
+import { Cinema } from './Cinema'
+import { Health } from './Health'
+
+export const NotFound = () => {
+  return (
+    <div>
+
+<Politics />
+    <Crime />
+    <Tech />
+    <Sports />
+    <Cinema />
+    <Health />
+    </div>
+  )
+}
